b5d889a4ca84d39ca50ca6e8cff299470c5f98c9
ben <ben.poon@microchip.com>
2022-11-29 14:33:31 +0800

    correct the folder path of the mem_def_workaround.bat

--- third_party/wfi32/mem_def_workaround.bat
+++ third_party/wfi32/mem_def_workaround.bat
@@ -1,4 +1,4 @@
-cp ./replace.vbs ../lwip/repo/lwip_test_2
-cd ../lwip/repo/lwip_test_2/
+cp ./replace.vbs ../lwip/repo/lwip
+cd ../lwip/repo/lwip/
 for /r %%i in (*) do cscript replace.vbs %%i "mem_free(" "mem_free_l("
 for /r %%i in (*) do cscript replace.vbs %%i "mem_free," "mem_free_l,"
